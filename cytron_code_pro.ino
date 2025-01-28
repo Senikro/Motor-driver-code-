1f30fa73999e201845b1783f2978b00fda550f7a
Senikro <152339689+Senikro@users.noreply.github.com>
2025-01-28 13:58:45 +0200

    Update cytron_code_pro.ino

--- cytron_code_pro.ino
+++ cytron_code_pro.ino
@@ -163,44 +163,155 @@ void movefrontdown(int speed) {
   analogWrite(MD_PWM, pwmSpeed); 
 
 }
+#define MAX_SPEED 228
+
+#define LU_PWM 2
+#define LU_DIR 30
+#define LD_PWM 3
+#define LD_DIR 32
+
+#define RU_PWM 4
+#define RU_DIR 34
+#define RD_PWM 5
+#define RD_DIR 36
+
+#define MU_PWM 6
+#define MU_DIR 38
+#define MD_PWM 7
+#define MD_DIR 40
+
+void setup() {
+  pinMode(LU_PWM, OUTPUT);
+  pinMode(LU_DIR, OUTPUT);
+  pinMode(RU_PWM, OUTPUT);
+  pinMode(RU_DIR, OUTPUT);
+  pinMode(LD_PWM, OUTPUT);
+  pinMode(LD_DIR, OUTPUT);
+  pinMode(RD_PWM, OUTPUT);
+  pinMode(RD_DIR, OUTPUT);
+  pinMode(MU_PWM, OUTPUT);
+  pinMode(MU_DIR, OUTPUT);
+  pinMode(MD_PWM, OUTPUT);
+  pinMode(MD_DIR, OUTPUT);
+}
+
+int calculateSpeed(int percentage) {
+  percentage = constrain(percentage, -100, 100); // Ensure percentage is within -100 to 100
+  int result; 
+  if (percentage>=100){
+    result = MAX_SPEED ;
+    }
+  else if (percentage<=100){
+    result = MAX_SPEED;
+    }
+  else {result = (abs(percentage) * MAX_SPEED) / 100;
+  }
+    return result;
+}
+
+void stopAll() {
+  analogWrite(LU_PWM, 0);
+  analogWrite(RU_PWM, 0);
+  analogWrite(LD_PWM, 0);
+  analogWrite(RD_PWM, 0);
+  analogWrite(MU_PWM, 0);
+  analogWrite(MD_PWM, 0);
+  delay(1500);
+}
+
+void y_axis(int percentage) {
+  int pwmSpeed = calculateSpeed(percentage);
+  if (percentage > 0) { // Move forward
+    digitalWrite(LU_DIR, HIGH);
+    digitalWrite(RU_DIR, HIGH);
+    digitalWrite(LD_DIR, LOW);
+    digitalWrite(RD_DIR, LOW);
+  } else { // Move backward
+    digitalWrite(LU_DIR, LOW);
+    digitalWrite(RU_DIR, LOW);
+    digitalWrite(LD_DIR, HIGH);
+    digitalWrite(RD_DIR, HIGH);
+  }
+  analogWrite(LU_PWM, pwmSpeed);
+  analogWrite(RU_PWM, pwmSpeed);
+  analogWrite(LD_PWM, pwmSpeed);
+  analogWrite(RD_PWM, pwmSpeed);
+}
+
+void x_axis(int percentage) {
+  int pwmSpeed = calculateSpeed(percentage);
+  if (percentage > 0) { // Move right
+    digitalWrite(LU_DIR, HIGH);
+    digitalWrite(RU_DIR, LOW);
+    digitalWrite(LD_DIR, HIGH);
+    digitalWrite(RD_DIR, LOW);
+  } else { // Move left
+    digitalWrite(LU_DIR, LOW);
+    digitalWrite(RU_DIR, HIGH);
+    digitalWrite(LD_DIR, LOW);
+    digitalWrite(RD_DIR, HIGH);
+  }
+  analogWrite(LU_PWM, pwmSpeed);
+  analogWrite(RU_PWM, pwmSpeed);
+  analogWrite(LD_PWM, pwmSpeed);
+  analogWrite(RD_PWM, pwmSpeed);
+}
+void tilt(int percentage) {
+  int pwmSpeed = calculateSpeed(percentage);
+  if (percentage > 0) { // TILT
+     digitalWrite(MU_DIR, HIGH);  
+      analogWrite(MU_PWM, pwmSpeed); 
+     digitalWrite(MD_DIR, LOW);  
+      analogWrite(MD_PWM, pwmSpeed); 
+  } else { // TILT
+    digitalWrite(MU_DIR, LOW);  
+     analogWrite(MU_PWM, pwmSpeed); 
+    digitalWrite(MD_DIR, HIGH);  
+     analogWrite(MD_PWM, pwmSpeed); 
+  }
+  analogWrite(MU_PWM, pwmSpeed);
+  analogWrite(MU_PWM, pwmSpeed);
+  analogWrite(MD_PWM, pwmSpeed);
+  analogWrite(MD_PWM, pwmSpeed);
+}
 void loop() {
-  moveForward(100); //from 0% to 100% 
+  y_axis(90); //from 0% to 100% 
   delay(2000);  
   stopAll();
 
-  moveBackward(150);//from 0% to 100% 
+  y_axis(-80);//from 0% to 100% 
   delay(2000);  
   stopAll();
 
-  moveForward(19);//from 0% to 100% 
+  x_axis(60);//from 0% to 100% 
   delay(2000);
   stopAll();
 
-  moveBackward(23);//from 0% to 100% 
+  x_axis(-50);//from 0% to 100% 
   delay(2000);
    stopAll();
 
-  moveRight(50);//from 0% to 100% 
+  tilt(45);//from 0% to 100% 
   delay(2000); 
   stopAll();
 
-  moveLeft(35);//from 0% to 100% 
+  tilt(-37);//from 0% to 100% 
   delay(2000);
   stopAll();
 
-  moveClock(80);//from 0% to 100% 
+  y_axis(93);//from 0% to 100% 
   delay(2000);
   stopAll();
 
-  moveAnti(69);//from 0% to 100% 
+  x_axis(76);//from 0% to 100% 
   delay(2000);
   stopAll();
 
-  movefrontup(73);//from 0% to 100% 
+  tilt(-120);//from 0% to 100% 
   delay(2000);
   stopAll();
 
-  movefrontdown(91);//from 0% to 100% 
+  tilt(69);//from 0% to 100% 
   delay(2000);
   stopAll();
 
